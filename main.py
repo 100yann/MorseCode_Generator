b4bb0fd985dc8fd100bf996ee32f424136bc61b8
Stoyan Kolev <stoyan.d.kolev@gmail.com>
2023-03-22 00:09:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 import customtkinter
 from PIL import Image
-import sqlalchemy
-from sqlalchemy import create_engine, text
+from sql import DB
+import random
+
+
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-'}
+
 
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
@@ -26,6 +44,7 @@ string_entry = customtkinter.CTkEntry(master=screen, width=250)
 string_entry.grid(column=2, row=2, padx=15)
 string_entry.focus()
 
+
 # Morse Field
 label_morse = customtkinter.CTkLabel(master=screen, text="Text in Morse")
 label_morse.grid(column=1, row=3, pady=10)
@@ -33,34 +52,17 @@ morse_entry = customtkinter.CTkEntry(master=screen, width=250)
 morse_entry.grid(column=2, row=3, padx=15)
 
 
-# Create SQLAlchemy engine
-engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
-
-# Create a temporary db for this instance, needs to be a specific db
-with engine.connect() as conn:
-    conn.execute(text("CREATE TABLE morse_db (Original_Input, Morse_Code)"))
-    conn.commit()
-
-def log_input(string, morse):
-    #store the original input and morse code into the temporary db
-    with engine.connect() as conn:
-        conn.execute(text("INSERT INTO morse_db (Original_Input, Morse_Code) VALUES (:Original_Input, :Morse_Code)"),
-                    [{"Original_Input": string, "Morse_Code": morse}]
-        )
-        conn.commit()
-    results()
+with open ("word_bank.txt", "r") as word_bank:
+    all_words = word_bank.readlines()
+    random_word = random.choice(all_words)
+    print(random_word)
 
 
-def results():
-    # shows if the original input and morse code were logged in properly
-    with engine.connect() as conn:
-        result = conn.execute(text("SELECT Original_Input, Morse_Code FROM morse_db"))
-        for row in result:
-            print(f"x: {row.Original_Input}  y: {row.Morse_Code}")
 
+# Load DB
+db = DB()
 
 def to_morse(string):
-  
     # Converts the string input into morse code
     morse_entry.delete(0, len(morse_entry.get()))
     dict_input = [char.title() for char in string]
@@ -69,9 +71,10 @@ def to_morse(string):
         if char in MORSE_CODE_DICT.keys():
             morse_string += f"{MORSE_CODE_DICT[char]} "
         elif char == " ":
-            morse_string += " " #Retains spacing 
+            morse_string += " " # Retains spacing 
     morse_entry.insert(0, morse_string)
-    log_input(string, morse_string)
+    # log the input/output to the db
+    db.log_input(string, morse_string)
 
 
 def to_human(morse_string):
@@ -98,26 +101,6 @@ button_2 = customtkinter.CTkButton(master=frame, text="To Human", command= lambd
 button_2.grid(column=2, row=4)
 
 
-MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
-                    'C':'-.-.', 'D':'-..', 'E':'.',
-                    'F':'..-.', 'G':'--.', 'H':'....',
-                    'I':'..', 'J':'.---', 'K':'-.-',
-                    'L':'.-..', 'M':'--', 'N':'-.',
-                    'O':'---', 'P':'.--.', 'Q':'--.-',
-                    'R':'.-.', 'S':'...', 'T':'-',
-                    'U':'..-', 'V':'...-', 'W':'.--',
-                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
-                    '1':'.----', '2':'..---', '3':'...--',
-                    '4':'....-', '5':'.....', '6':'-....',
-                    '7':'--...', '8':'---..', '9':'----.',
-                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
-                    '?':'..--..', '/':'-..-.', '-':'-....-',
-                    '(':'-.--.', ')':'-.--.-'}
-
-
-
 screen.mainloop()
 
-# Added a temporary DB that stores the input and morse code generated in this instance of the code, need to
-# connect to a permanent db that stores the information, not one that creates a new db table on each 
-# instance of the code. I would also work on separating the functionality into different files
+
